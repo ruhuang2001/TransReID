8b5f38c4f800f143ff1bf87bb55d607cb789b35a
ruhuang <ruhuang2001@gmail.com>
2024-04-25 15:04:09 +0800

    Fix the torch._six not found in torch version 2

--- model/backbones/vit_pytorch.py
+++ model/backbones/vit_pytorch.py
@@ -27,13 +27,14 @@ from itertools import repeat
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch._six import container_abcs
+from collections.abc import Iterable
+
 
 
 # From PyTorch internals
 def _ntuple(n):
     def parse(x):
-        if isinstance(x, container_abcs.Iterable):
+        if isinstance(x, Iterable):
             return x
         return tuple(repeat(x, n))
     return parse
